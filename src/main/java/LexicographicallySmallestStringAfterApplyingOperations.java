59b374f3f68558411ac81fab213e1b85527ca835
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2025-10-19 23:53:10 +0530

    Lexicographically Smallest String After Applying Operations

---
+++ src/main/java/LexicographicallySmallestStringAfterApplyingOperations.java
@@ -0,0 +1,33 @@
+import java.util.*;
+// Definition for singly-linked list.
+public class LexicographicallySmallestStringAfterApplyingOperations {
+    public static void main(String[] args) {
+        String s="5525";
+        int a = 9,  b = 2;
+        System.out.println(findLexSmallestString(s,a,b));
+    }
+
+        public static String findLexSmallestString(String s, int a, int b) {
+            Set<String> vis = new HashSet<>();
+            String smallest = s;
+            Deque<String> q = new ArrayDeque<>();
+            q.offer(s);
+            vis.add(s);
+
+            while (!q.isEmpty()) {
+                String cur = q.poll();
+                if (cur.compareTo(smallest) < 0) smallest = cur;
+
+                StringBuilder sb = new StringBuilder(cur);
+                for (int i = 1; i < sb.length(); i += 2)
+                    sb.setCharAt(i, (char) ((sb.charAt(i) - '0' + a) % 10 + '0'));
+                String added = sb.toString();
+                if (vis.add(added)) q.offer(added);
+
+                String rotated = cur.substring(cur.length() - b) + cur.substring(0, cur.length() - b);
+                if (vis.add(rotated)) q.offer(rotated);
+            }
+            return smallest;
+        }
+
+}
