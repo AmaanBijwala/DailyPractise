73e98b6a7efdf12bdd8aa1c2ae3e117868c067d4
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2025-11-27 23:58:52 +0530

    MaximumSubarraySumWithLengthDivisibleByK

---
+++ src/main/java/MaximumSubarraySumWithLengthDivisibleByK.java
@@ -0,0 +1,27 @@
+package BasicMath;
+import java.util.*;
+class MaximumSubarraySumWithLengthDivisibleByK{
+
+    public static void main(String args[]){
+        int [] nums={1,2};
+        int k=1;
+    }
+
+
+    public static  long maxSubarraySum(int[] nums, int k) {
+        long[] minPref = new long[k];
+        Arrays.fill(minPref, Long.MAX_VALUE / 2);
+        minPref[k - 1] = 0;
+
+        long sum = 0, res = Long.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int r = i % k;
+            res = Math.max(res, sum - minPref[r]);
+            minPref[r] = Math.min(minPref[r], sum);
+        }
+
+        return res;
+    }
+}
