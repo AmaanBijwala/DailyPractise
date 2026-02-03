fe38601873c1156f2553c63bc6c22b5df184695e
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2026-02-03 22:20:57 +0530

    Trionic Array I

---
+++ src/main/java/TrionicArrayI.java
@@ -0,0 +1,27 @@
+
+public class TrionicArrayI {
+
+        public boolean isTrionic(int[] nums) {
+            int n=nums.length;
+            int i=0;
+            while(i+1<n && nums[i+1]>nums[i]){
+                i++;
+            }
+            if(i==0 || i==n-1){
+                return false;
+            }
+            while(i+1<n && nums[i+1]<nums[i]){
+                i++;
+            }
+            if(i==n-1){
+                return false;
+            }
+            while(i+1<n && nums[i+1]>nums[i]){
+                i++;
+            }
+            if(i==n-1)
+                return true;
+            else
+                return false;
+        }
+    }
