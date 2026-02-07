63f7f940bdbe341704ce6b8a51a6b0e36a3670ce
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2026-02-07 23:00:59 +0530

    Minimum Deletions to Make String Balanced

---
+++ src/main/java/MinimumDeletionsToMakeStringBalanced.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class MinimumDeletionsToMakeStringBalanced {
+
+    public int minimumDeletions(String s) {
+        int n=s.length();
+        int count=0;
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(!st.isEmpty() && s.charAt(i)=='a' && st.peek()=='b'){
+                st.pop();
+                count++;
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        return count;
+    }
+}
