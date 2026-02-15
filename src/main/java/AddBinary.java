981c043d6b8c05a8ce03f932ced1820230c3f023
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2026-02-15 14:03:54 +0530

    Add Binary

---
+++ src/main/java/AddBinary.java
@@ -0,0 +1,11 @@
+
+import java.math.BigInteger;
+public class AddBinary {
+
+    public String addBinary(String a, String b) {
+        BigInteger val1 = new BigInteger(a, 2);
+        BigInteger val2 = new BigInteger(b, 2);
+        BigInteger sum = val1.add(val2);
+        return sum.toString(2);
+    }
+}
